85c87eff3d76d0d89a4c2a416d038b1c76346456
sushant-bhat <sushant.bhat.102@gmail.com>
2022-07-25 00:13:17 +0530

    Design change

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 var hamMenu = document.getElementById("ham-menu")
+
+const headerHeight = document.querySelector("header").offsetHeight;
+document.documentElement.style.setProperty("--scroll-padding", headerHeight + 20 + "px");
