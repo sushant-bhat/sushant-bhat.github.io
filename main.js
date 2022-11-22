88ee0e7f76bb870fd3557ce7824ea9da8070176e
s1b030z <sushant.bhat@walmartlabs.com>
2022-11-22 19:09:58 +0530

    Hide/show header while scrolling

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-// var hamMenu = document.getElementById("ham-menu")
+const header = document.querySelector("header");
+const rootElem = document.querySelector(':root');
+rootElem.style.setProperty('--header-height', -1 * header.offsetHeight + "px");
+let lastScrollY = window.scrollY;
 
-// const headerHeight = document.querySelector("header").offsetHeight;
-// document.documentElement.style.setProperty("--scroll-padding", headerHeight + 20 + "px");
-console.log("Hello");
+window.addEventListener("scroll", () => {
+    console.log(header.offsetHeight);
+    if (lastScrollY < window.scrollY) {
+        header.classList.add("header-hide");
+    } else {
+        header.classList.remove("header-hide");
+    }
+    lastScrollY = window.scrollY;
+});
