a4b87f002f851b5a539f8f7a7c657e9174729542
s1b030z <sushant.bhat@walmartlabs.com>
2023-04-20 21:40:03 +0530

    Revamped experience page

---
+++ js/experience.js
@@ -0,0 +1,17 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+let expItems = document.querySelectorAll('.exp__img')
+let expInfos = document.querySelectorAll('.exp__info > li')
+
+expItems.forEach(expItem => observer.observe(expItem))
+expInfos.forEach(expInfo => observer.observe(expInfo))
+
