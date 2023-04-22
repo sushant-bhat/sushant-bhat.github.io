8c29a6575eb18c2c487a9f35347b00e22e263983
sushant-bhat <sushant.bhat.102@gmail.com>
2023-04-22 14:28:19 +0530

    Fixed experience page and changed nav link styling

--- js/experience.js
+++ js/experience.js
@@ -10,8 +10,9 @@ const observer = new IntersectionObserver((entries) => {
 })
 
 let expItems = document.querySelectorAll('.exp__img')
-let expInfos = document.querySelectorAll('.exp__info > li')
+let expInfos = document.querySelectorAll('.exp__info')
+let expInfoItems = document.querySelectorAll('.exp__info > li')
 
 expItems.forEach(expItem => observer.observe(expItem))
 expInfos.forEach(expInfo => observer.observe(expInfo))
-
+expInfoItems.forEach(expInfoItem => observer.observe(expInfoItem))
