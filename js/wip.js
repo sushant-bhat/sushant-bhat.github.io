0958b8754f4d3be48998342ea1c90935b90d4bf3
s1b030z <sushant.bhat@walmartlabs.com>
2023-05-24 08:40:03 +0530

    Added animations to WIP

---
+++ js/wip.js
@@ -0,0 +1,17 @@
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }
+    })
+})
+
+let wipTitles = document.querySelectorAll('.wip__desc h3')
+wipTitles.forEach(title => {
+    observer.observe(title)
+})
+
+let wipDescPara = document.querySelectorAll('.wip__desc p')
+wipDescPara.forEach(para => {
+    observer.observe(para)
+})
